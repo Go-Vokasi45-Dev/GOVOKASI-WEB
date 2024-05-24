a184f3678e8cb64afe23c95f3dcf457db5a5b66c
Naufal-Tech <naufal.geo11@gmail.com>
2024-05-24 09:16:49 +0700

    add WA

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToFooter() {
+  document.getElementById("footer").scrollIntoView({ behavior: "smooth" });
+}
+
+function redirectToWhatsApp() {
+  whatsappUrl = "https://wa.me/6281316653083";
+  window.open(whatsappUrl, "_blank");
+}
